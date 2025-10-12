f90e4ef0ef292cb05fed84717c031f764cde7af0
20vikash <thisisforspotifyvikash@gmail.com>
2025-10-12 13:35:06 +0530

    Check before configuring prefetch counter

--- pool.go
+++ pool.go
@@ -2,7 +2,6 @@ package mqpool
 
 import (
 	"errors"
-	"log"
 
 	mqerrors "github.com/20vikash/mqpool/internal/errors"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -87,16 +86,17 @@ func (p *channelPool) PushChannel(ch *amqp.Channel) error {
 //
 // PrefetchCounter ensures the number of messages to send to the consumer before acks.
 //
-// PrefetchCounter will be ignored if the channel is going to be used for Producing
+// PrefetchCounter will be ignored if the channel is going to be used for Producing.
 func (p *channelPool) GetFreeChannel(prefetchCounter int) (*amqp.Channel, error) {
 	ch := <-p.Pool
 
 	if ch.IsClosed() { // Close in other part of code, or broker closed it
 		newCh, err := p.Conn.Channel()
-		newCh.Qos(prefetchCounter, 0, false)
 		if err != nil {
-			log.Println("Cannot create a new channel")
 			return nil, err
+		}
+		if prefetchCounter > 0 {
+			newCh.Qos(prefetchCounter, 0, false)
 		}
 		return newCh, nil
 	}
